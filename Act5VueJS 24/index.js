731d182bb1c18c026e16d8f77e3de33e10d8cd80
DarioRivas <dario.rivas@gmail.com>
2021-06-28 15:45:36 -0300

    28-06

---
+++ Act5VueJS 24/index.js
@@ -0,0 +1,62 @@
+const mispropiedades = {
+    data() {
+        return {
+            nombre: '',
+            apellido: '',
+            dni:'',
+            cuota:'',
+            correo:'',
+            importe:'',
+            fecha:'',
+            listaPagos: []
+        }
+    },
+    methods: {
+        guardarPago() {
+            const nuevoPago = {
+                nombre: this.nombre,
+                apellido: this.apellido,
+                dni: this.dni,
+                cuota: this.cuota,
+                correo: this.correo,
+                importe: this.importe,
+                fecha: this.fecha
+            }
+            if ("tareas" in localStorage) {
+                this.listaPagos = JSON.parse(localStorage.getItem("pagos"));
+                this.listaPagos.push(nuevoPago);
+                localStorage.setItem("pagos",JSON.stringify(this.listaPagos));
+            } else {
+                this.listaPagos.push(nuevoPago);
+                localStorage.setItem("pagos",JSON.stringify(this.listaPagos));
+            }
+            this.nombre = '';
+            this.apellido = '';
+            this.dni = '';
+            this.cuota = '';
+            this.correo = '';
+            this.importe = '';
+            this.fecha = '';
+        },
+        listarPagos(){
+            if("pagos" in localStorage){
+                this.listaPagos = JSON.parse(localStorage.getItem("pagos"));
+            }else{
+                this.listaPagos = [];
+            }
+        },
+        eliminarPago(index){
+            this.listaPagos.splice(index,1);
+            localStorage.setItem("pagos",JSON.stringify(this.listaPagos));
+        },
+        vaciarLocal(){
+            localStorage.removeItem("pagos");
+            this.listarPagos();
+        }
+    },
+mounted() {
+    this.listarPagos();
+
+}
+}
+Vue.createApp(mispropiedades).mount("#app");
