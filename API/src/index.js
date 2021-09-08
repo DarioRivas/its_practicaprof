0f46b871c0573d9325dcf9c4f21114ebbc8ddceb
DarioRivas <dario.rivas@gmail.com>
2021-09-08 16:28:47 -0300

    08-09

--- API/src/index.js
+++ API/src/index.js
@@ -1,22 +1,24 @@
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
-//aca obtengo toda la funcionalidad de express, depositada en la constante server
-const server = express()
+const express = require('express') //Trajimos la lib.
+const cors = require('cors') // Trajimos la lib.
+const morgan = require('morgan') // Traigo la lib.
 
-//especificamos el formato de datos que va  aminuplas nuestros servidor
+const server = express() //Alojo la función express que traje de node_modules, declarada arriba
+
+//especificamos el formato de datos que va a manipular nuestro servidor, es decir, nuestra API
 server.use(express.json())
 
-//nos va a permitir comunicar al cliente con el servidor y viceversa
-server.use(cors)
+//Cors nos permite comunicar al cliente con el servidor y viceversa.
+server.use(cors())
 
-//nos va a notificar en la consola cada ves que se haga una petision http
+// morgan nos permite notificar por consola las peticiones de HTTP(put,delete,post,get(Todas forman CRUDE))
 server.use(morgan('dev'))
 
-//asigno un puerto:
-server.set('port', process.env.PORT || 3001)
+// el process.env.PORT le digo que el predeterminado sea el 3000, si no, que lea y busque algun puerto abierto.
+server.set('port', process.env.PORT || 3000) // Estoy seteando la variable 'port', dentro de express, donde lo quiero especficiar. En este caso el puerto 3000
 
-//le damos arranque a nuestro servidor
+//Poner las rutas en funcionamiento
+server.use(require('./routes/cliente.route'))
+
+//Le damos arranque a nuestro servidor
 server.listen(server.get('port'))
-
-console.log(`Servidor corriendo en el puerto ${server.get('port')}`)
+console.log(`Servidor corriendo en el puerto: ${server.get('port')}`)
